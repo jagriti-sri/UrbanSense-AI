0bcc652baaf6dd25f8378dd9643dc03e3b149669
Samriddhi Shrivastava <samriddhishrivastav19@gmail.com>
2026-03-28 14:18:26 +0530

    Added QR system, waste tracking, and image upload feature

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-
+from routes.qr_routes import qr_bp
 from routes.auth_routes import auth_bp
 from routes.complaint_routes import complaint_bp
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.config['JWT_SECRET_KEY'] = 'secret123'
 
 CORS(app)
 jwt = JWTManager(app)
-
+app.register_blueprint(qr_bp, url_prefix='/qr')
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(complaint_bp, url_prefix='/complaint')
 # ✅ ADD THIS ROUTE
